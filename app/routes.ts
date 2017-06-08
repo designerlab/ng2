099a2c81ff8879e8b2e248b4fcefe9bc23a4c4bb
sridharswaminathan <sridhar.snathan@mimos.my>
2017-06-08 23:53:11 +0800

    updated error pages

--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router'
 import { EventsListComponent } from './events/events-list.component'
 import { EventDetailsComponent } from './events/event-details/event-details.component'
+import { CreateEventComponent } from './create-event.component'
+import { Error404Component } from './error/404.component'
+import { DummyComponent } from './dummy/dummy.component'
+import { EventRouterActivator } from './events/event-details/event-router-activator.service'
 
 export const appRoutes:Routes = [
+    {path: 'events/new', component: CreateEventComponent, canDeactivate:['canDeactivateCreateEvent'] },
     {path: 'events', component: EventsListComponent },
-    {path: 'events/:id', component: EventDetailsComponent },
+    {path: 'events/:id', component: EventDetailsComponent, canActivate:[EventRouterActivator] },
+    {path: '404', component: Error404Component},
+    {path: 'dummy', component: DummyComponent},
     {path: '', redirectTo: '/events', pathMatch: 'full'}
 ]
