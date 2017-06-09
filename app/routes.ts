7272f580ab1307f3eb658a67079be3821acda088
Sridhar Swaminathan <sridhar.snathan@mimos.my>
2017-06-09 15:34:16 +0800

    updated user module

--- app/routes.ts
+++ app/routes.ts
@@ -13,5 +13,6 @@ export const appRoutes:Routes = [
     {path: 'public/new', component: CreateEventComponent, canDeactivate:['canDeactivateCreateEvent'] },
     {path: 'public', component: EventsListComponent, resolve:{events: EventListResolver} },
     {path: 'public/:id', component: EventDetailsComponent, canActivate:[EventRouterActivator] },
-    {path: '', redirectTo: '/public', pathMatch: 'full'}
+    {path: '', redirectTo: '/public', pathMatch: 'full'},
+    {path: 'user', loadChildren: 'app/user/user.module#UserModule'}
 ]
