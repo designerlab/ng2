29c2e3ac96092a24167f81cdefe89da11053f44a
Sridhar Swaminathan <sridhar.snathan@mimos.my>
2017-06-19 10:11:59 +0800

    updated

--- app/header/nav/navbar.component.ts
+++ app/header/nav/navbar.component.ts
@@ -9,8 +9,15 @@ import { IMenu, IUrl } from './nav.model'
     selector: 'nav-bar',
     templateUrl: './app/header/nav/navbar.component.html',
     styles:[`
-        .nav.navbar-nav {font-size:15px;}
+        
+        .nav.navbar-nav {font-size:15px; padding:10px 0px;}
         li > a.active {color:#F97924;}
+        .nav > li{
+            position: relative;
+            display: inline-block;
+            margin-top: -10px;
+            height: 60px;
+        }
         #searchForm { margin-right:100px;}
         @media (max-width:1200px){#searchForm {display:none;} }
         .top-nav{
@@ -22,10 +29,26 @@ import { IMenu, IUrl } from './nav.model'
         }
         .navbar-default .navbar-nav>li>a{
             color:#000 !important;
+            padding: 20px;
+            color:#333333;
+            display:block;
+            padding:0px 40px;
+            text-decoration:none;
+            float: left;
+            height: 60px;
+            line-height: 60px;
         }
 
         .navbar-default .navbar-nav>li>a:hover{
             color:#fff !important;
+        }
+
+        .navbar-brand {
+            float: left;
+            padding: 10px 15px;
+            font-size: 29px;
+            line-height: 40px;
+            height: 60px;
         }
        
     `]
