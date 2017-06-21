06cb469b24c150061c65dd081aa50483d1400a08
sridharswaminathan <sridhar.snathan@mimos.my>
2017-06-21 21:50:59 +0800

    updated

--- app/events-app.component.ts
+++ app/events-app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core'
-//import { TranslateService } from 'ng2-translate'
 import {TranslateService} from '@ngx-translate/core';
 @Component({
     selector: 'events-app',
